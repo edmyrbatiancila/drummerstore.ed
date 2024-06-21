0d7a92790436517f833466de9c0ead5700cd2b32
edmyrbatiancila <batiancilaedmyr@gmail.com>
2024-06-22 02:24:13 +0800

    updated code and added new html file.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
+const smallImgCol = document.getElementsByClassName("small-img-col");
 
 if(bar) {
     bar.addEventListener('click', () => {
@@ -11,5 +12,24 @@ if(bar) {
 if(close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
+    });
+}
+
+// Change main image and add border on small image click
+var mainImg = document.getElementById("MainImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+for (let i = 0; i < smallImg.length; i++) {
+    smallImg[i].addEventListener('click', function() {
+        // Change the main image source
+        mainImg.src = smallImg[i].src;
+
+        // Remove border from all small images
+        for (let j = 0; j < smallImgCol.length; j++) {
+            smallImgCol[j].style.border = "none";
+        }
+
+        // Add border to the clicked small image
+        smallImgCol[i].style.border = "2px solid #FF8C00";
     });
 }
